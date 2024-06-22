26d1634b31931cd8db80a184e6c645b105a44245
j-oshi <jtoshin@msn.com>
2024-06-22 16:07:49 +0100

    remove test files

--- agents/agent.py
+++ agents/agent.py
@@ -4,7 +4,7 @@ import json
 from importlib import import_module
 
 # Set directory paths
-parent_dir = os.path.abspath('.')
+parent_dir = os.path.abspath('..')
 if parent_dir not in sys.path:
     sys.path.append(parent_dir)
 
@@ -12,15 +12,11 @@ from prompts.modify_prompt import system_prompt_template
 from model_api import ollama_model_api, openai_model_api
 from registry.ai_assistants_registry import registry as registered_ai_assistants
 
-# from ai_assistants.assistants_registry import get_functions_list
-# from ai_assistants.assistants.financial.fixed_rate import fixed_rate_prompt
-
 # Define supported model APIs (assuming these are defined elsewhere)
 SUPPORTED_MODEL_APIS = {
     "ollama_model_api": ollama_model_api.OllamaAPI,
     "openai_model_api": openai_model_api.OpenaiAPI,
 }
-
 
 class Agent:
     def __init__(self, model_api_key, model_name, ai_assistants):
@@ -54,17 +50,13 @@ class Agent:
             return self.execute_function(func_name, func_input_str)
 
     def execute_function(self, func_name, input_str):
-        return self.ai_assistants.execute_function(func_name, input_str)
-        # Use import_module for dynamic tool loading
-        # for tool in self.tools:
-        #     if tool.__name__ == func_name:
-        #         return tool(input_str)
-        # # Raise an error if no matching tool is found
-        # raise ValueError(f"Function not found: {func_name}")
-
+        try:
+            return self.ai_assistants.execute_function(func_name, input_str)
+        except Exception as e:
+            raise ValueError(f"Error executing function '{func_name}': {e}")
 
 if __name__ == "__main__":
-    model_api_key = "ollama_model_api"  # Assuming this is the chosen API
+    model_api_key = "ollama_model_api"
     model_name = "mistral:latest"
     ai_assistants = registered_ai_assistants
 
@@ -73,17 +65,16 @@ if __name__ == "__main__":
         model_name=model_name,
         ai_assistants=ai_assistants,
     )
-    # response = agent.execute_prompt('What is capital of the united states?')
-    # response = agent.execute_prompt(
-    #     "If a principal amount of $5000 is invested at a fixed annual interest rate of 2.5% for a period of 3 years, what is the totalCostOfMortgage?"
-    # )
-    # print(response)
 
     while True:
         prompt = input("Enter prompt here: ")
         if prompt.lower() == "exit":
             break
     
-        response = agent.execute_prompt(prompt)
-        print(response)
+        try:
+            response = agent.execute_prompt(prompt)
+            print(response)
+        except ValueError as e:
+            print(f"An error occurred: {e}")
+
 
--- registry/my_functions.py
+++
@@ -1,8 +0,0 @@
-def add(x, y):
-    return x + y
-
-def double(x):
-    return x * 2
-
-def test():
-    print('I am good thank you.')
