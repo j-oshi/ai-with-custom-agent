3c9b0dc78b075b53c064c00711f9d1bbb9c675ae
j-oshi <jtoshin@msn.com>
2024-07-28 06:52:35 +0100

    update

--- tools/web/connection.py
+++ tools/web/connection.py
@@ -2,6 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 def check_website_connectivity(input_str):
     """
@@ -21,7 +27,7 @@ def check_website_connectivity(input_str):
         response = requests.head(url, timeout=5)
         return response.status_code == 200
     except requests.RequestException:
-        return response.status_code
+        return f'Server error code is {response.status_code}.'
 
 
 def get_website_content(input_str):
@@ -47,6 +53,50 @@ def get_website_content(input_str):
         soup = BeautifulSoup(response.text, 'html.parser')
         return soup.prettify()
     except requests.RequestException as e:
+        return f"An error occurred: {e}"
+    
+    
+def get_web_content_headless(input_str):
+    """
+    Get the content of a website with an headless browser.
+
+    Args:
+        input_str (dict): A dictionary containing the URL of the website to retrieve content from.
+
+    Returns:
+        str: The content of the website.
+    """
+    try:
+        url = input_str.get('url')
+        if not url:
+            raise ValueError("The input dictionary must contain a 'url' key with a valid URL.")
+        
+        # Set up Chrome options
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")  # Run in headless mode
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+
+        # Initialize the WebDriver
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+
+        # Open the URL
+        driver.get(url)
+
+        # Wait for the page to load completely
+        driver.implicitly_wait(10)
+
+        # Get the page source
+        page_source = driver.page_source
+
+        # Close the WebDriver
+        driver.quit()
+
+        # Parse the page source with BeautifulSoup
+        soup = BeautifulSoup(page_source, 'html.parser')
+        return soup.prettify()
+    except Exception as e:
         return f"An error occurred: {e}"
 
 
